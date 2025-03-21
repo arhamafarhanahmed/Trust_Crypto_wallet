90308402a72a96bad9085407e6e247cf06221f56
shaikhshayan2005 <140731752+shaikhshayan2005@users.noreply.github.com>
2025-03-21 20:13:10 +0500

    commit_main

--- my-project/Client/src/App.jsx
+++ my-project/Client/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-// import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import PlatformFeatures from './Components/PlateformFeautures'
-import CryptoSection from './Components/CryptoSection'
-import SecuritySection from './Components/SecuritySection'
-import WalletSection from './Components/WalletSection'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import PlatformFeatures from "./Components/PlateformFeautures";
+import CryptoSection from "./Components/CryptoSection";
+import SecuritySection from "./Components/SecuritySection";
+import WalletSection from "./Components/WalletSection";
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
-      <Hero/>
-      <PlatformFeatures/>
-      <CryptoSection/>
-      <SecuritySection/>
-      <WalletSection/>
+      <Navbar />
+      <Hero />
+      <PlatformFeatures />
+      <CryptoSection />
+      <SecuritySection />
+      <WalletSection />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
