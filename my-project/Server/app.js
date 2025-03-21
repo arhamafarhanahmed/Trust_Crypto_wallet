7558a729a0fb2765bc46f5fcfa6716c932a873be
shaikhshayan2005 <140731752+shaikhshayan2005@users.noreply.github.com>
2025-03-21 12:16:26 +0500

    changes_Server_created

---
+++ my-project/Server/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+require("./database/connection");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// const cors = require("cors");
+
+app.use(express.json());
+// app.use(cors());
+// app.use("/api", router);
+
+const port = 8005;
+
+app.listen(port, () => {
+  console.log(`server is connected to ${port}`);
+});
